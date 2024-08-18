0e907318ceac52ecf577bec4ed3bccddd8b7f4c9
lsc892 <extraordinary2549@gmail.com>
2024-08-18 17:14:24 +0900

    [ADD] 오브젝트 SAVE&LOAD기능 추가

--- Assets/Scripts/AY/SaveManager.cs
+++ Assets/Scripts/AY/SaveManager.cs
@@ -6,7 +6,7 @@ public class SaveManager : Singleton<SaveManager>
 {
     public List<QuestSO> savedQuest = new List<QuestSO>();
     public List<DoorInteraction> savedDoors = new List<DoorInteraction>();
-    public List<ObjectInteraction> saveItemsScript = new List<ObjectInteraction>();
+    public List<ObjectInteractionStatus> saveItemsStatus = new List<ObjectInteractionStatus>();
 
     public List<GameObject> savedItems = new List<GameObject>();
 
@@ -27,17 +27,17 @@ public class SaveManager : Singleton<SaveManager>
         }
         savedQuest.Clear();
         savedDoors.Clear();
-        saveItemsScript.Clear();
+        saveItemsStatus.Clear();
         EventManager.Instance.PostNotification(Event_Type.eSave, this);
     }
 
     public void Load()
     {
+        Debug.Log("로드 실행");
         if (savedItems.Count > 0)
         {
             GameObject saveItem = savedItems[savedItems.Count - 1];
             saveItem.SetActive(true);
-            Debug.Log("오브젝트 퀘스트이름"+ saveItemsScript[savedItems.Count - 1].GetComponent<ObjectInteraction>().questName);
             savedItems.RemoveAt(savedItems.Count - 1);
         }
         EventManager.Instance.PostNotification(Event_Type.eLoad, this);
